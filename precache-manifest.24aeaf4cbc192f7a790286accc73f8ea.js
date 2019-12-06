a44a565d2e6a92e7cb59dfde02fa92084358fc76
andrey5608 <andrey5608@gmail.com>
2019-12-06 22:21:53 +0300

    Updates

---
+++ precache-manifest.24aeaf4cbc192f7a790286accc73f8ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed5d7a50b10869c920d826cfe25271c",
+    "url": "/sensorFrontApp/index.html"
+  },
+  {
+    "revision": "70937a57e605b9553492",
+    "url": "/sensorFrontApp/static/css/main.4fc8a2e1.chunk.css"
+  },
+  {
+    "revision": "74913e262b6233a49cad",
+    "url": "/sensorFrontApp/static/js/2.553f6f0f.chunk.js"
+  },
+  {
+    "revision": "70937a57e605b9553492",
+    "url": "/sensorFrontApp/static/js/main.2316a46b.chunk.js"
+  },
+  {
+    "revision": "83e34e15a8b67f85f96b",
+    "url": "/sensorFrontApp/static/js/runtime-main.1ec177f4.js"
+  }
+]);
